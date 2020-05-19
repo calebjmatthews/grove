c05029b13194bc3063c8fa9020d4ffbe2dbf271e
Caleb Matthews <calebjmatthews@gmail.com>
2020-05-19 09:13:45 -0500

    Test map buttons

--- client/game_states/edit/map_buttons.ts
+++ client/game_states/edit/map_buttons.ts
@@ -1,24 +1,45 @@
 import { map } from '../../instances/map';
+import { pieceTypes } from '../../instances/piece_types';
+
+const sceneNames = ["../../editing/scene1.json"];
+let scenes = {};
+import("../../editing/scene1.json").then((json) => {
+  scenes["../../editing/scene1.json"] = json;
+});;
 
 export function createMapButtons() {
   let ele = document.getElementById('foot');
   let buttonContainer = document.createElement("DIV");
   buttonContainer.setAttribute("class", "button-container");
 
-  let loadButton = document.createElement("DIV");
-  loadButton.setAttribute("class", "button");
-  loadButton.appendChild(document.createTextNode('Load'));
-  loadButton.addEventListener("click", () => {
-    console.log('map');
-    console.log(map);
-  });
-  buttonContainer.appendChild(loadButton);
+  sceneNames.map((sceneName) => {
+    let loadButton = document.createElement("DIV");
+    loadButton.setAttribute("id", sceneName);
+    loadButton.setAttribute("class", "button");
+    loadButton.appendChild(document.createTextNode('Load ' + sceneName));
+    loadButton.addEventListener("click", () => {
+      console.log('scenes');
+      console.log(scenes);
+      console.log('sceneName');
+      console.log(sceneName);
+    });
+    buttonContainer.appendChild(loadButton);
+  })
 
   let saveButton = document.createElement("DIV");
+  saveButton.setAttribute("id", "save_button");
   saveButton.setAttribute("class", "button");
   saveButton.appendChild(document.createTextNode('Save'));
   saveButton.addEventListener("click", () => {
-    console.log('save!');
+    let expObj = {
+      "pieceMap": map.pieceMap
+    };
+    let aEle = document.createElement("a");
+    let file = new Blob([JSON.stringify(expObj)], {type : 'application/json'});
+    aEle.href = URL.createObjectURL(file);
+    aEle.download = 'scene.json';
+    aEle.click();
+    URL.revokeObjectURL(aEle.href);
   });
   buttonContainer.appendChild(saveButton);
 
