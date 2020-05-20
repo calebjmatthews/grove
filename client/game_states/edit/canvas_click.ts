56c4d2c43d796541d00fcfa07c6f593326a861d8
Caleb Matthews <calebjmatthews@gmail.com>
2020-05-20 15:32:51 -0500

    Edit mode piece replacement to use destroyPiece

--- client/game_states/edit/canvas_click.ts
+++ client/game_states/edit/canvas_click.ts
@@ -13,38 +13,44 @@ export function handleCanvasClick() {
         let gridPos = map.getGridUpperLeftPos([(ev.x - map.offset.x),
           (ev.y - map.offset.y)])
         let selPiece = map.getPieceByGridPos(gridPos);
+        console.log('selPiece');
+        console.log(selPiece);
 
-        if (selPiece.animated) {
-          delete map.piecesAnimated[selPiece.name + ',' + selPiece.id];
-        }
-        else {
-          delete map.pieces[selPiece.name + ',' + selPiece.id];
-        }
-        delete map.pieceMap[gridPos[0] + ',' + gridPos[1]];
+        map.destroyPiece(selPiece, pixiContainers, sprites);
+
+        // if (selPiece.animated) {
+        //   delete map.piecesAnimated[selPiece.name + ',' + selPiece.id];
+        // }
+        // else {
+        //   delete map.pieces[selPiece.name + ',' + selPiece.id];
+        // }
+        // delete map.pieceMap[gridPos[0] + ',' + gridPos[1]];
 
         let newId = Math.floor(Math.random() * 10000000);
-        let newName = pieceTypeNameSel + ',' + newId;
-        let newPiece = pieceTypes[pieceTypeNameSel].createPiece(newId, gridPos,
-          [selPiece.box.x, selPiece.box.y]);
-
-        let dSprite = new PIXI.Sprite(PIXI.utils.TextureCache[newPiece.spriteNames[0]]);
-        dSprite.x = newPiece.box.x;
-        dSprite.y = newPiece.box.y;
-        dSprite.width = newPiece.box.width;
-        dSprite.height = newPiece.box.height;
-        sprites[newName] = dSprite;
-        pixiContainers[PieceTypeNames.BACKGROUND].addChild(dSprite);
-
-        if (newPiece.animated) {
-          map.piecesAnimated[newName] = newPiece;
-          map.pieceMap[(gridPos[0] + ',' + gridPos[1])] = { mapName: 'piecesAnimated',
-            pieceName: newName };
-        }
-        else {
-          map.pieces[newName] = newPiece;
-          map.pieceMap[(gridPos[0] + ',' + gridPos[1])] = { mapName: 'pieces',
-            pieceName: newName };
-        }
+        map.createAndDisplayPiece(pieceTypeNameSel, (gridPos[0] + ',' + gridPos[1]),
+          newId, pieceTypes, pixiContainers, sprites);
+        // let newName = pieceTypeNameSel + ',' + newId;
+        // let newPiece = pieceTypes[pieceTypeNameSel].createPiece(newId, gridPos,
+        //   [selPiece.box.x, selPiece.box.y]);
+        //
+        // let dSprite = new PIXI.Sprite(PIXI.utils.TextureCache[newPiece.spriteNames[0]]);
+        // dSprite.x = newPiece.box.x;
+        // dSprite.y = newPiece.box.y;
+        // dSprite.width = newPiece.box.width;
+        // dSprite.height = newPiece.box.height;
+        // sprites[newName] = dSprite;
+        // pixiContainers[PieceTypeNames.BACKGROUND].addChild(dSprite);
+        //
+        // if (newPiece.animated) {
+        //   map.piecesAnimated[newName] = newPiece;
+        //   map.pieceMap[(gridPos[0] + ',' + gridPos[1])] = { mapName: 'piecesAnimated',
+        //     pieceName: newName };
+        // }
+        // else {
+        //   map.pieces[newName] = newPiece;
+        //   map.pieceMap[(gridPos[0] + ',' + gridPos[1])] = { mapName: 'pieces',
+        //     pieceName: newName };
+        // }
       }
   });
 }
