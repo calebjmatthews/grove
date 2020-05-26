cdda7984b8b63da27de8f4e6bacd6c21b844ceaa
Caleb Matthews <calebjmatthews@gmail.com>
2020-05-26 09:16:01 -0500

    Palatte to use images rather than text

--- client/game_states/edit/palatte.ts
+++ client/game_states/edit/palatte.ts
@@ -1,6 +1,8 @@
 import { pieceTypes } from '../../instances/piece_types';
 import { setPieceTypeSelect } from './piece_type_select';
 
+
+
 export function createPalatte() {
   let ele = document.getElementById('foot');
   let palatte = document.createElement("DIV");
@@ -10,8 +12,10 @@ export function createPalatte() {
     let div = document.createElement("DIV");
     div.setAttribute("id", pieceType.name);
     div.setAttribute("class", "palette-item");
-    let textNode = document.createTextNode(pieceType.name);
-    div.appendChild(textNode);
+    let img = document.createElement("IMG");
+    img.setAttribute("src", ("dist/working/forest/" + pieceType.spriteNames[0]));
+    // let textNode = document.createTextNode(pieceType.name);
+    div.appendChild(img);
     div.addEventListener("click", () => {
       setPieceTypeSelect(pieceType.name);
     });
