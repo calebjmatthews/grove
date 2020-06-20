c3396c957648612f8500b2c3d8d0aae344ce9648
Caleb Matthews <calebjmatthews@gmail.com>
2020-06-20 14:42:21 -0500

    Scene editing to work with pixi-tilemap

--- client/game_states/edit/edit.ts
+++ client/game_states/edit/edit.ts
@@ -3,9 +3,13 @@ import { animate } from '../play/animate';
 import { applyOffset } from '../play/apply_offset';
 import { map } from '../../instances/map';
 import { sprites } from '../../instances/sprites';
+import { pixiRenderer } from '../../instances/pixi_renderer';
+import { pixiContainers } from '../../instances/pixi_containers';
+const pc = pixiContainers;
 
 export default function edit(delta: number) {
-  animate();
+  // animate();
   applyOffset(delta);
   map.animateParticleGroups(delta);
+  pixiRenderer.pr.render(pc.all);
 }
